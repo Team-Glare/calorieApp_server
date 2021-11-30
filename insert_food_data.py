e19b0d35c4948af697750cc1b123936c32c645e3
mjsohack <mjsohack@ncsu.edu>
2021-11-29 23:29:40 -0500

    Inserted food data to atlas

--- insert_food_data.py
+++ insert_food_data.py
@@ -1,6 +1,18 @@
-from apps import App
-app = App()
-mongo = app.mongo
+import yaml
+from flask import Flask
+from flask_pymongo import PyMongo
+
+app = Flask(__name__)
+app.secret_key = 'secret'
+
+# app.config['MONGO_URI'] = 'mongodb://127.0.0.1:27017/test'
+app.config['MONGO_CONNECT'] = True
+with open('application.yml') as f:
+    info = yaml.load(f, Loader=yaml.FullLoader)
+    username = info['username']
+    password = info['password']
+    app.config['MONGO_URI'] = f'mongodb+srv://{username}:{password}@apptracker.goffn.mongodb.net/calorieApp?retryWrites=true&w=majority'
+mongo = PyMongo(app)
 
 f = open('food_data/calories.csv', 'r', encoding = "ISO-8859-1")
 l = f.readlines()
